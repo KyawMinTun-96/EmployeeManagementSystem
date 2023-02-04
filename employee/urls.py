c5cfa478a05627538381fbb9dfb6a99ce475ca89
mgkyawmin <kyawmintun.kmt4142020@gmail.com>
2023-02-04 14:20:21 +0630

    2023-02-04 modify design

--- employee/urls.py
+++ employee/urls.py
@@ -2,13 +2,16 @@ from django.urls import URLPattern
 
 
 from . import views
+from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
-
-
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
-    path('', views.home, name="HomePage"),
+    path('', views.home, name="home-page"),
+    path('departments', views.departments, name="department-page"),
+    path('positions', views.positions, name="position-page"),
+    path('employees', views.employees, name="employee-page"),
+    path('projects', views.projects, name="project-page"),
 ]
--- employee/views.py
+++ employee/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 
 
 # from django.shortcuts import redirect, render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 # from employee_information.models import Department, Position, Employees
 # from django.contrib.auth import authenticate, login, logout
 # from django.contrib.auth.decorators import login_required
-# from django.shortcuts import redirect
+from django.shortcuts import redirect
 # import json
 
 
@@ -16,9 +16,33 @@ from django.shortcuts import render
 def home(request):
     context = {
         'page_title':'Home',
-    #     # 'employees':employees,
-    #     'total_department':len(Department.objects.all()),
-    #     'total_position':len(Position.objects.all()),
-    #     'total_employee':len(Employees.objects.all()),
     }
     return render(request, 'ems_info/home.html', context)
+
+
+def departments(request):
+    context = {
+        'page_title':'Department',
+    }
+    return render(request, 'ems_info/departments.html', context)
+
+
+def projects(request):
+    context = {
+        'page_title' : 'Project',
+    }
+    return render(request, 'ems_info/projects.html', context)
+
+
+def positions(request):
+    context = {
+        'page_title' : 'Position',
+    }
+    return render(request, 'ems_info/positions.html', context)
+
+
+def employees(request):
+    context = {
+        'page_title' : 'Employee',
+    }
+    return render(request, 'ems_info/employees.html', context)
